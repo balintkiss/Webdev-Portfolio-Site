d50745e6397b56c0a83cc9525a1e53b3a69012e7
balintkiss <baleintkiss@gmail.com>
2024-07-02 11:55:11 +0200

    Updated Files

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyA2hKnej0hzeCxCRaD_rBFPpRj0GhzdQ1c",
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-  });
+document.getElementById('home-btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-one').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('about-btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-two').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('services-btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-four').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact-btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-five').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('service-btn').addEventListener('click', function() {
+  document.getElementById('main-four').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('home-footer').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-one').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('services-footer').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-four').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact-footer').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-five').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('aboutus-footer').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  document.getElementById('main-two').scrollIntoView({ behavior: 'smooth' });
+});
+
+ű
